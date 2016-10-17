26814e01e2c112d4ada4f04178320ce14a087eb9
Damon <damon@mahuika.co.nz>
2016-10-18 02:42:44 +0900

    Unused import

--- src/Drawer/DrawerDomain.js
+++ src/Drawer/DrawerDomain.js
@@ -1,4 +1,4 @@
-import { observable, autorun, computed, action } from 'mobx'
+import { observable, computed, action } from 'mobx'
 import { app } from '@mindhive/di'
 import { WindowSize } from '../responsiveUi/windowMetrics'
 
