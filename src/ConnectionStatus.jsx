608eadac4d0c7ab53298f1b07d5b5c8bcd29f957
Damon <damon@mahuika.co.nz>
2016-10-18 18:06:22 +0900

    Remove use of vocab to keep generic

--- src/ConnectionStatus.jsx
+++ src/ConnectionStatus.jsx
@@ -1,17 +1,15 @@
 import { observer } from 'mobx-react'
 import React from 'react'
-import { connect } from 'react-redux'
 import FlatButton from 'material-ui/FlatButton'
 import { app } from '@mindhive/di'
 
 
 const ConnectionStatus = ({
   domains: { connectionDomain } = app(),
-  vocab,
 }) =>
   connectionDomain.connectionDown &&
     <div style={{ background: 'red', color: 'white' }}>
-      Can't connect to {vocab.App}, will keep trying. Check your Internet connection.
+      Can't connect the the server, will keep trying. Check your Internet connection.
       {connectionDomain.hasPendingCalls &&
         <div>
           Changes you've made are waiting to be sent, and will be lost unless you reconnect.
@@ -26,12 +24,8 @@ const ConnectionStatus = ({
     </div>
 
 
-const mapStateToProps = ({ vocab }) => ({
-  vocab,
-})
-
-export default connect(mapStateToProps)(
+export default
   observer(
     ConnectionStatus
   )
-)
+
