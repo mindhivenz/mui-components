6c52d8ad48d01a7614b023ee1dfd383144719fce
nickvirtue <nick@mahuika.co.nz>
2016-11-17 16:40:39 +1300

    New Drawer Components

--- src/AppLayout/AppLayout.jsx
+++ src/AppLayout/AppLayout.jsx
@@ -63,7 +63,8 @@ const calcStylesStyles = (
     paddingTop: spacing.desktopKeylineIncrement,
     transition: `${transitions.easeOut(null, 'padding-left', null)},
                   ${transitions.easeOut(null, 'width', null)}`,
-    paddingLeft: navDrawerDomain.docked ? navDrawerDomain.expanded ? drawer.expandedWidth : drawer.narrowWidth : 0
+    paddingLeft: navDrawerDomain.docked ? drawer.expandedWidth : 0
+    // paddingLeft: navDrawerDomain.docked ? navDrawerDomain.expanded ? drawer.expandedWidth : drawer.narrowWidth : 0
   },
 })
 
