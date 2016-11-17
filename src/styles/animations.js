f13ff0faa6110d5b2bea0c73f65179d60cb822c5
nickvirtue <nick@mahuika.co.nz>
2016-11-18 01:05:38 +1300

    New Drawer Components LayoutDomain

--- src/styles/animations.js
+++ src/styles/animations.js
@@ -7,6 +7,7 @@ export const CUBIC_BEZIER = 'cubic-bezier(0.23, 1, 0.32, 1)'
 
 export const trans = {
   cubicAll: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+  cubicAllFor: ms => `all ${ms}ms cubic-bezier(0.23, 1, 0.32, 1) 0ms`,
 }
 
 export default {
