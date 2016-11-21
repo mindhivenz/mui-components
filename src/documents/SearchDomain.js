793ef22810d8e1ef0106cc6e9302edd5c47b3f4e
nickvirtue <nick@mahuika.co.nz>
2016-11-22 12:08:31 +1300

    SearchDomain

--- src/documents/SearchDomain.js
+++ src/documents/SearchDomain.js
@@ -1,13 +1,15 @@
 import { action, observable, computed } from 'mobx'
 
-export class SearchDomain {
+
+
+class SearchDomain {
 
   @observable
   searchText = ''
 
-  constructor({source = [], filters = []}) {
-    this.source = source
-    this.filters = filters
+  constructor({items = [], retrievers = []}) {
+    this.items = items
+    this.retrievers = retrievers
   }
 
   @action
@@ -15,14 +17,13 @@ export class SearchDomain {
     this.searchText = searchText
   }
 
-  @computed get filtered() {
-    return this.source.filter(s => {
-      let wanted = false
-      this.filters.forEach(f => {
-        wanted = wanted || f(s).toLowerCase().includes(this.searchText.toLowerCase())
-      })
-      return wanted
-    })
+  @computed get results() {
+    return this.items.filter(item =>
+      this.retrievers.reduce(
+        (includeItem, retrieve) => includeItem || retrieve(item).toLowerCase().includes(this.searchText.toLowerCase()),
+        false
+      )
+    )
   }
 
 }
