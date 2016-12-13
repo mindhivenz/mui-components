fc484f9b484ba493f0499124b18a5d4257a751a1
Damon <damon@mahuika.co.nz>
2016-12-13 19:14:29 +0900

    redux-form validators into this package

--- reduxForm/index.js
+++ reduxForm/index.js
@@ -1,5 +1,23 @@
 var error = require('../dist/reduxForm/error')
+var validators = require('../dist/reduxForm/validators')
 
 module.exports = {
   asSubmissionError: error.asSubmissionError,
+  join: validators.join,
+  createFormValidator: validators.createFormValidator,
+  isEmpty: validators.isEmpty,
+  words: validators.words,
+  alphaNumeric: validators.alphaNumeric,
+  email: validators.email,
+  required: validators.required,
+  requiredIf: validators.requiredIf,
+  minLength: validators.minLength,
+  maxLength: validators.maxLength,
+  integer: validators.integer,
+  oneOf: validators.oneOf,
+  uniqueName: validators.uniqueName,
+  match: validators.match,
+  containsUpper: validators.containsUpper,
+  containsLower: validators.containsLower,
+  containsNumber: validators.containsNumber,
 }
