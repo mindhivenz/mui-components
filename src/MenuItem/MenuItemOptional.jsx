d1d8fcb8d5803f1b99dff02f6f24a7bec461f686
nickvirtue <nick@mahuika.co.nz>
2016-12-21 10:07:39 +1300

    Styling for MenuItemOptional

--- src/MenuItem/MenuItemOptional.jsx
+++ src/MenuItem/MenuItemOptional.jsx
@@ -5,14 +5,18 @@ import withTheme from '../theme/withTheme'
 
 const MenuItemOptional = ({
   label = 'None',
+  style, // passed by MUI
 
   theme,
+
+  ...other,
 }) =>
   <MenuItem
     value={null}
     primaryText={label}
     label={null}
-    style={{ color: theme.palette.disabledColor }}
+    style={Object.assign({}, style, { color: theme.palette.disabledColor })}
+    {...other}
   />
 
 export default withTheme(MenuItemOptional)
