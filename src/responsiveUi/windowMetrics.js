d7aca64b0b9ae1906a2453594ca55f56c860b217
Damon <damon@mahuika.co.nz>
2017-01-02 02:14:16 +0700

    As per Material Design spec account for window chrome in desktop sizes

--- src/responsiveUi/windowMetrics.js
+++ src/responsiveUi/windowMetrics.js
@@ -1,9 +1,11 @@
 import { Enum } from 'enumify'
 
 
+const DESKTOP_CHROME_ALLOWANCE = 16  // As per spec: https://material.io/guidelines/layout/responsive-ui.html#responsive-ui-breakpoints
+
 export class WindowSize extends Enum {}
 
-// https://www.google.com/design/spec/layout/responsive-ui.html#responsive-ui-breakpoints
+// https://material.io/guidelines/layout/responsive-ui.html#responsive-ui-breakpoints
 // Keep in size order
 WindowSize.initEnum({
   X_SMALL: {
@@ -13,10 +15,10 @@ WindowSize.initEnum({
     breakpoint: 960,
   },
   MEDIUM: {
-    breakpoint: 1280,
+    breakpoint: 1280 - DESKTOP_CHROME_ALLOWANCE,
   },
   LARGE: {
-    breakpoint: 1920,
+    breakpoint: 1920 - DESKTOP_CHROME_ALLOWANCE,
   },
   X_LARGE: {},
 })
