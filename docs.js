ec07845ea42237fa847459f912069b79adfaf468
Damon <damon@mahuika.co.nz>
2017-03-30 13:37:19 +0700

    Moving selectedState and SELECT_NEW to @mindhive/meteor

--- docs.js
+++ docs.js
@@ -1,8 +1,5 @@
-var url = require('./dist/documents/url')
 var DocEdit = require('./dist/documents/DocEdit')
 
 module.exports = {
-  selectedState: url.selectedState,
-  SELECT_NEW_ID: url.SELECT_NEW_ID,
   withDocEditContext: DocEdit.withDocEditContext,
 }
