f07072b53f18736fa04c9c30a7cc6a2c9477b07c
nickvirtue <nick@mahuika.co.nz>
2017-05-23 17:43:08 +1200

    Allow background images for app container

--- src/AppLayout/AppLayout.jsx
+++ src/AppLayout/AppLayout.jsx
@@ -50,8 +50,7 @@ const mapThemeToStyles = (
     margin: '0 auto',
     transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
 
-    color: container.color,
-    backgroundColor: container.backgroundColor,
+    ...container,
 
   },
   content: {
