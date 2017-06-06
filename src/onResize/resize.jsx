e3dac525bb0cf5bec85dd594c30f3ef2ccaa8a9e
nickvirtue <nick@mahuika.co.nz>
2017-06-06 17:58:49 +1200

    Refactor onResize lifecycle

--- src/onResize/resize.jsx
+++ src/onResize/resize.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import debounce from 'lodash/debounce'
+import { compose, lifecycle } from 'recompose'
+
 
 import WatchElementResize from './core/base'
 
@@ -9,12 +11,13 @@ class Watcher {
 
   _width = undefined
   _height = undefined
+  _node = undefined
 
   constructor(onResize, {name, echoOnly} = {name: defaultWatcherName, echoOnly: false}) {
     this.onResize = onResize
     this.name = name
     this.echoOnly = echoOnly
-    if (this.name !== defaultWatcherName) console.log(`Create resize watcher ${this.name}`, this)
+    if (this.name !== defaultWatcherName) console.log(`Create resize watcher ${this.name}`)
   }
 
   _onResizeEvent = debounce((evt) => {
@@ -34,19 +37,53 @@ class Watcher {
   _resizeProps = () => ({[this.name]: this})
 
   registerNode = (node) => {
-    if (node) {
-      this.watcher = new WatchElementResize(node).on('resize', this._onResizeEvent)
-    } else if (this.watcher) {
-      if (this.name !== defaultWatcherName) console.log(`Remove resize watcher ${this.name}`)
-      this.watcher.removeListener()
+    this._node = node
+    this._createWatcher()
+  }
+
+  _createWatcher = () => {
+    if (this._node) {
+      this.watcher = new WatchElementResize(this._node).on('resize', this._onResizeEvent)
     }
   }
+
+  didMount = () => {
+    // if (this.name !== defaultWatcherName) console.log(`${this.name} didMount ${this._node}`)
+    this._createWatcher()
+  }
+
+  willUnmount = () => {
+    // if (this.name !== defaultWatcherName) console.log(`${this.name} willUnmount  ${this._node}`)
+    if (this.watcher) {
+      if (this.name !== defaultWatcherName) console.log(`Remove resize watcher ${this.name}`)
+      this.watcher.removeListener()
+      this.watcher = undefined
+    }
+  }
+
 }
+
+
 
 export default (onResize, options) => Component => (props = {}) => {
   const resizeWatcher = new Watcher(({top, left, width, height}, watcher) => onResize({ top, left, width, height }, props, watcher), options)
+
+  const MountDetector = compose(
+    lifecycle({
+      componentDidMount() {
+        resizeWatcher.didMount()
+      },
+      componentWillUnmount() {
+        resizeWatcher.willUnmount()
+      },
+    }),
+  )(() => null)
+
   return (
-    <Component {...props} {...resizeWatcher._resizeProps()} />
+    <div>
+      <MountDetector />
+      <Component {...props} {...resizeWatcher._resizeProps()} />
+    </div>
   )
 }
 
